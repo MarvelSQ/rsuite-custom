b637aa456cb210d7503e414c30670787f584af9e
MarvelSQ <sun1993hlj@hotmail.com>
2023-03-18 15:43:42 +0800

    fix(locale): 修复 noResultsText 展示效果

--- src/features/locale/NoResultText.tsx
+++ src/features/locale/NoResultText.tsx
@@ -1,60 +1,147 @@
-import { Divider } from "rsuite";
+import { useLayoutEffect, useRef } from "react";
+import { Divider, PickerHandle } from "rsuite";
 import CodePreview from "../../components/CodePreview";
+import { Components } from "../../components/OverrideComponent";
+import { mockInput } from "../../utils/mockInput";
+
+function waitFor(callback: () => boolean, ms: number = 100) {
+  let resolve = (v: any) => {};
+  let reject = (v: any) => {};
+  const promise = new Promise((res, rej) => {
+    resolve = res;
+    reject = rej;
+  });
+  let attempts = 0;
+  const timer = setInterval(() => {
+    if (callback()) {
+      clearInterval(timer);
+      resolve(null);
+    }
+    attempts++;
+    if (attempts > 10) {
+      clearInterval(timer);
+      reject(new Error("Timeout"));
+    }
+  }, ms);
+  return promise;
+}
+
+function AutoSearch(Component: React.ComponentType) {
+  const isInputPicker = Component === Components.InputPicker;
+  const searchInputSelector = isInputPicker
+    ? "input.rs-picker-search-input"
+    : "input.rs-picker-search-bar-input";
+  return (props: any) => {
+    const pickerRef = useRef<PickerHandle>(null);
+
+    useLayoutEffect(() => {
+      let target: HTMLElement | null = null;
+      waitFor(() => {
+        if (isInputPicker) {
+          target = pickerRef.current?.root || null;
+          return !!target;
+        }
+        try {
+          target = pickerRef.current?.overlay || null;
+          return !!target;
+        } catch (e) {
+          return false;
+        }
+      }).then(() => {
+        const input = (target as HTMLElement).querySelector(
+          searchInputSelector
+        ) as HTMLInputElement;
+        if (input) {
+          mockInput(input, "test");
+        }
+      });
+    }, []);
+    return <Component {...props} open ref={pickerRef} />;
+  };
+}
+
+const overrideComponents = Object.entries(Components)
+  .filter(([key]) => {
+    return key.endsWith("Picker") || key.endsWith("Cascader");
+  })
+  .reduce((acc, [key, comp]) => {
+    const Comp = comp as React.ComponentType;
+    acc[key] = AutoSearch(Comp);
+    return acc;
+  }, {} as Record<string, React.ComponentType<any>>) as any as typeof Components;
 
 function NoResultText() {
   return (
     <div>
-      <h4>Picker Component</h4>
+      <p>
+        This text appears when the user enters a search keyword that does not
+        find a corresponding result
+      </p>
+      <h4>Cascader</h4>
       <CodePreview
-        code={`<Form>
-  <Form.Group>
-    <Form.ControlLabel>Cascader</Form.ControlLabel>
-    <Form.Control name="name" accepter={Cascader} open data={[]} />
-  </Form.Group>
-  <Form.Group>
-    <Form.ControlLabel>CheckPicker</Form.ControlLabel>
-    <Form.Control name="name" accepter={CheckPicker} />
-  </Form.Group>
-  <Form.Group>
-    <Form.ControlLabel>CheckTreePicker</Form.ControlLabel>
-    <Form.Control name="name" accepter={CheckTreePicker} />
-  </Form.Group>
-  <Form.Group>
-    <Form.ControlLabel>DatePicker</Form.ControlLabel>
-    <Form.Control name="name" accepter={DatePicker} />
-  </Form.Group>
-  <Form.Group>
-    <Form.ControlLabel>DateRangePicker</Form.ControlLabel>
-    <Form.Control name="name" accepter={DateRangePicker} />
-  </Form.Group>
-  <Form.Group>
-    <Form.ControlLabel>InputPicker</Form.ControlLabel>
-    <Form.Control name="name" accepter={InputPicker} />
-  </Form.Group>
-  <Form.Group>
-    <Form.ControlLabel>MultiCascader</Form.ControlLabel>
-    <Form.Control name="name" accepter={MultiCascader} />
-  </Form.Group>
-  <Form.Group>
-    <Form.ControlLabel>SelectPicker</Form.ControlLabel>
-    <Form.Control name="name" accepter={SelectPicker} />
-  </Form.Group>
-  <Form.Group>
-    <Form.ControlLabel>TreePicker</Form.ControlLabel>
-    <Form.Control name="name" accepter={TreePicker} />
-  </Form.Group>
-</Form>
+        className=" h-48"
+        override={overrideComponents}
+        code={`<div>
+  <p>Cascader</p>
+  <Cascader data={[]} />
+</div>
 `}
       />
       <Divider />
-      <h4>RadioGroup</h4>
-      <CodePreview code={`<div>选项组：<RadioGroup plaintext /></div>`} />
+      <h4>CheckPicker</h4>
+      <CodePreview
+        className=" h-48"
+        override={overrideComponents}
+        code={`<div>
+  <p>CheckPicker</p>
+  <CheckPicker data={[]} />
+</div>
+`}
+      />
       <Divider />
-      <h4>Rate</h4>
-      <CodePreview code={`<div>评分: <Rate plaintext value={null} /></div>`} />
+      <h4>CheckTreePicker</h4>
+      <CodePreview
+        className=" h-48"
+        override={overrideComponents}
+        code={`<div>
+  <p>CheckTreePicker</p>
+  <CheckTreePicker data={[]} />
+</div>
+`}
+      />
       <Divider />
-      <h4>Slider</h4>
-      <CodePreview code={`<div>滑动条: <Slider plaintext /></div>`} />
+      <h4>InputPicker</h4>
+      <CodePreview
+        className="h-32"
+        override={overrideComponents}
+        code={`<div>
+  <p>InputPicker</p>
+  <InputPicker data={[]} />
+</div>
+`}
+      />
+      <Divider />
+      <h4>MultiCascader</h4>
+      <CodePreview
+        className=" h-48"
+        override={overrideComponents}
+        code={`<div>
+  <p>MultiCascader</p>
+  <MultiCascader data={[]} />
+</div>
+`}
+      />
+      <Divider />
+      <h4>SelectPicker</h4>
+      <CodePreview
+        className=" h-48"
+        override={overrideComponents}
+        code={`<div>
+  <p>SelectPicker</p>
+  <SelectPicker data={[]} />
+</div>
+`}
+      />
     </div>
   );
 }
